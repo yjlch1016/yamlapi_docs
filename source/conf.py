95cc305089eeb6211d75427a1c4c3d1708435fde
yangjianliang <526861348@qq.com>
2021-08-28 16:51:33 +0800

    提交V1.4.0的说明文档

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, yangjianliang'
 author = 'yangjianliang'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.4.0'
 
 # -- General configuration ---------------------------------------------------
 
